42b7b5375c1e5f3a20715faf1d9f8b83e4b45fc9
edu4rdo-rodrigues <eduardo28455@gmail.com>
2023-08-11 19:56:15 -0300

    first commit

---
+++ graphql/resolvers.ts
@@ -0,0 +1,10 @@
+import { Resolvers } from "./resolvers-types";
+
+export const resolvers: Resolvers = {
+  Query: {
+    links: async (parent, args, ctx) => {
+      const results = await ctx.prisma.link.findMany();
+      return results;
+    }
+  }
+}
